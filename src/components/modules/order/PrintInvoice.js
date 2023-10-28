4b21bbbd92b0b35f9296ea3b5d37e69dba073ae2
Rabby <rabby410@gmail.com>
2023-10-28 15:56:29 +0600

    Print Invoice design

--- src/components/modules/order/PrintInvoice.js
+++ src/components/modules/order/PrintInvoice.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Barcode from 'react-barcode';
+import Barcode from "react-barcode";
 import GlobalFunction from "../../../assets/GlobalFunction";
 
 const tableCellStyle = {
@@ -12,10 +12,13 @@ const tdCellStyle = {
   padding: "8px",
 };
 const rightAlignedTdCellStyle = {
-    ...tdCellStyle,
-    textAlign: "right",
-  };
-  
+  ...tdCellStyle,
+  textAlign: "right",
+};
+const leftAlignedTdCellStyle = {
+  textAlign: "left",
+};
+
 const PrintInvoice = ({ order }) => {
   return (
     <div style={{ textAlign: "center" }}>
@@ -37,12 +40,25 @@ const PrintInvoice = ({ order }) => {
           </center>
           <div id="mid">
             <div class="info">
-                <h5>{order?.shop?.address?.address}</h5>
-                <p className="m-0">
-                Email: {order?.shop?.email}<br/>
-                Contact: {order?.shop?.phone}
-                </p>
-                <p className="bordered">Vat Reg: 004782306-0202</p>
+              {/* <h5>{order?.shop?.address?.address}</h5> */}
+              <div style={leftAlignedTdCellStyle}>
+                Govt. of the People's Republic of Bangladesh. National Board of
+                Revenue Tax Invoice (MUSAK-6.3)
+                <br />
+                [Clauses(c)&(f) of Sub-rules(1) of Rule 40]
+                <br />
+                BIN: 004782306-0202
+                <br />
+                {order?.shop?.name}
+              </div>
+              <div style={leftAlignedTdCellStyle}>
+                <br />
+                User: {order?.sales_manager?.name}
+                <br />
+                Date: {order?.created_at}
+                <br />
+                Receipt No: {order?.order_number}
+              </div>
             </div>
           </div>
           <table style={tableCellStyle}>
@@ -60,13 +76,17 @@ const PrintInvoice = ({ order }) => {
                 <tr key={index}>
                   <td style={tdCellStyle}> {++index}</td>
                   <td style={tdCellStyle}>
-                    <p>{product.name}<br /> {product.sku}</p>
+                    <p>
+                      {product.name}
+                      <br /> {product.sku}
+                    </p>
                   </td>
                   <td style={tdCellStyle}>{product?.quantity}</td>
                   <td style={tdCellStyle}>
-                    <p>Price : {product?.price}
-                    <br/>
-                    Discount :{" "}
+                    <p>
+                      Price : {product?.price}
+                      <br />
+                      Discount :{" "}
                       {GlobalFunction.formatPrice(
                         product?.sell_price?.discount
                       )}
@@ -80,20 +100,59 @@ const PrintInvoice = ({ order }) => {
                 </tr>
               ))}
               <tr>
-                <td colSpan={4} style={rightAlignedTdCellStyle}>Total</td>
-                <td style={tdCellStyle}>{GlobalFunction.formatPrice(order?.total)}</td>
+                <td colSpan={4} style={rightAlignedTdCellStyle}>
+                  Total
+                </td>
+                <td style={tdCellStyle}>
+                  {GlobalFunction.formatPrice(order?.total)}
+                </td>
               </tr>
             </tbody>
           </table>
         </div>
-        <p style={{ textAlign: "center" }}><Barcode value={order?.order_number} width={2} format="CODE128" height={30} font="monospace"/></p>
-        <div style={{ textAlign: "center" }}>
-          <p>
-            Contact: {order?.shop?.phone}
+        <div style={leftAlignedTdCellStyle}>
+          <p className="text-uppercase">
+            Mode of Payment <br /> .................................
+            <br />
+            <span>{order?.payment_method?.name}</span> :{" "}
+            {GlobalFunction.formatPrice(order?.paid_amount)}
           </p>
-          <p>Note: Thank you for Coming to Hometex Bangladesh & we look forward to serve you again!</p>
-          <p>Please visit: https://hometex.ltd/ VAT payable at Center Registration</p>
         </div>
+        <div style={leftAlignedTdCellStyle}>
+          <p className="text-uppercase">
+            Customer:
+            <br />
+            Name: {order?.customer?.name}
+            <br />
+            Contact: {order?.customer?.phone}
+          </p>
+          ...................................
+        </div>
+        <div style={leftAlignedTdCellStyle}>
+          <p>
+            Items may be exchanged subject to Hometex Bangladesh Sales &
+            Exchange Policies within 15 days of Invoice Date. An item may be
+            exchanged only once. An item must be unworn, unwashed, undamaged and
+            unused [Fairly Enough Condition]. No Cash Refund is Applicable.
+            <br />
+            ...................................
+            <br />
+            VAT Note: 7.5% VAT on ready-made products. No VAT on jute items.
+            <br />
+            Shop Online: www.hometex.ltd Queries & Complaints: +8809610963839
+            <br/>
+            Suggestions: support@hometex.ltd
+          </p>
+        </div>
+        <p style={{ textAlign: "center" }}>
+          <Barcode
+            value={order?.order_number}
+            width={2}
+            format="CODE128"
+            height={30}
+            font="monospace"
+          />
+        </p>
       </div>
     </div>
   );
