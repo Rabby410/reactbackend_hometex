688b55e249eefed78d5572795dd9bbd4b936e1bb
rabby home <rabby410@gmail.com>
2023-12-15 23:55:04 +0600

    barcode page 1 issue

--- src/components/modules/bar_code/BarCode.js
+++ src/components/modules/bar_code/BarCode.js
@@ -3,71 +3,59 @@ import axios from "axios";
 import Breadcrumb from "../../partoals/Breadcrumb";
 import Constants from "../../../Constants";
 import CardHeader from "../../partoals/miniComponents/CardHeader";
-import { useReactToPrint } from "react-to-print"; // Import the hook
+import { useReactToPrint } from "react-to-print";
 import BarCodePage from "./BarCodePage";
 import { useLocation } from "react-router-dom";
 
 const BarCode = () => {
+  const componentRef = useRef();
   const location = useLocation();
+  const productSKU = location?.state?.productSKU;
+  const [columnCount, setColumnCount] = useState(1);
   const [selectedAttribute, setSelectedAttribute] = useState(null);
   const [attributes, setAttributes] = useState([]);
-  const productSKU = location?.state?.productSKU;
-  const productName = location?.state?.productName;
-  const componentRef = useRef();
-  const [columnCount, setColumnCount] = useState(1);
   const [input, setInput] = useState({
     name: "",
     sub_category_id: "",
     category_id: "",
+    attribute_value_id: "",
   });
   const [isLoading, setIsLoading] = useState(false);
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
   const [subCategories, setSubCategories] = useState([]);
+
   useEffect(() => {
     setInput((prevInput) => ({
       ...prevInput,
       name: productSKU || "",
     }));
   }, [productSKU]);
-  
-  // Add this useEffect block to set the initial selectedAttribute
-  useEffect(() => {
-    if (products.length > 0) {
-      setAttributes(products[0]?.product_attributes || []);
-      const selectedAttr = products[0]?.product_attributes[0] || null;
-      setSelectedAttribute(selectedAttr);
-    } else {
-      setAttributes([]);
-      setSelectedAttribute(null);
-    }
-  }, [products]);
-  
 
   const handleInput = (e) => {
     if (e.target.name === "category_id") {
-        let category_id = parseInt(e.target.value);
-        if (!Number.isNaN(category_id)) {
-            getSubCategories(e.target.value);
-        }
+      let category_id = parseInt(e.target.value);
+      if (!Number.isNaN(category_id)) {
+        getSubCategories(e.target.value);
+      }
     }
 
     setInput((prevState) => ({
-        ...prevState,
-        [e.target.name]: e.target.value,
+      ...prevState,
+      [e.target.name]: e.target.value,
     }));
-    
-    // Find the selected attribute based on the attribute_value_id
-    const selectedAttr = attributes.find(attr => attr.attribute?.id === parseInt(input.attribute_value_id));
-    setSelectedAttribute(selectedAttr);
 
-    setAttributes([]);
-};
+    // Update the selected attribute based on the input attribute_value_id
+    const selectedAttr = attributes.find(
+      (attr) => attr.id === parseInt(e.target.value, 10)
+    );
 
+    setSelectedAttribute(selectedAttr || null);
+  };
 
-  const handleProductSearch = () => {
+  const handleProductSearch = async () => {
     const token = localStorage.getItem("token");
-    axios
+    await axios
       .get(
         `${Constants.BASE_URL}/get-product-list-for-bar-code?name=${input?.name}&category_id=${input?.category_id}&sub_category_id=${input?.sub_category_id}`,
         {
@@ -78,27 +66,43 @@ const BarCode = () => {
       )
       .then((res) => {
         setProducts(res.data.data);
-  
-        // Set attributes for the first product in the result
+
         if (res.data.data.length > 0) {
-          const productAttributes = res.data.data[0]?.product_attributes || [];
-          console.log("Product Attributes:", productAttributes);
-          setAttributes(productAttributes);
-          // Set selected attribute based on the first product's attributes
-          const selectedAttr = productAttributes[0] || null;
-          setSelectedAttribute(selectedAttr);
+          const firstProduct = res.data.data[0];
+
+          if (
+            firstProduct.product_attributes &&
+            firstProduct.product_attributes.length > 0
+          ) {
+            setAttributes(firstProduct.product_attributes);
+            setSelectedAttribute(
+              firstProduct.product_attributes.find(
+                (attr) =>
+                  attr.id === parseInt(input.attribute_value_id, 10)
+              ) || firstProduct.product_attributes[0]
+            );
+          } else {
+            setAttributes([]);
+            setSelectedAttribute(null);
+          }
         } else {
-          // Reset attributes if no products are found
           setAttributes([]);
           setSelectedAttribute(null);
-          console.log("No products found");
         }
-      })
-      .catch((error) => {
-        // Handle API request errors, e.g., show an error message to the user.
-        console.error("Error fetching data:", error)
-      })
+      });
   };
+
+  useEffect(() => {
+    if (products.length > 0) {
+      setAttributes(products[0]?.product_attributes || []);
+      const selectedAttr = products[0]?.product_attributes[0] || null;
+      setSelectedAttribute(selectedAttr);
+    } else {
+      setAttributes([]);
+      setSelectedAttribute(null);
+    }
+  }, [products]);
+
   const getCategories = () => {
     const token = localStorage.getItem("token");
     axios
@@ -129,23 +133,20 @@ const BarCode = () => {
     getCategories();
   }, []);
 
-  // Define the print function
   const handlePrint = useReactToPrint({
     content: () => componentRef.current,
     documentTitle: "Bar Codes",
   });
-  const selectedAttributeObject = attributes.find(attr => attr.id === selectedAttribute);
-console.log(input.attribute_value_id, "aaa")
 
   return (
     <>
       <style>
         {`
-                @page {
-                    size: auto;
-                    margin: 0;
-                }
-                `}
+          @page {
+              size: auto;
+              margin: 0;
+          }
+        `}
       </style>
       <Breadcrumb title={"Generate or Create Barcode"} />
       <div className="row">
@@ -163,7 +164,7 @@ console.log(input.attribute_value_id, "aaa")
 
             <div className="card-body">
               <div className="row align-items-baseline">
-                <div className="col-md-2">
+                <div className="col-md-3">
                   <label className="w-100 mt-4 mt-md-0">
                     <p>Select Product Category</p>
                     <select
@@ -171,7 +172,6 @@ console.log(input.attribute_value_id, "aaa")
                       name={"category_id"}
                       value={input.category_id}
                       onChange={handleInput}
-                      placeholder={"Select Product Category"}
                     >
                       <option>Select Category</option>
                       {categories.map((category, index) => (
@@ -182,7 +182,7 @@ console.log(input.attribute_value_id, "aaa")
                     </select>
                   </label>
                 </div>
-                <div className="col-md-2">
+                <div className="col-md-3">
                   <label className="w-100 mt-4 mt-md-0">
                     <p>Select Product Sub Category</p>
                     <select
@@ -190,7 +190,6 @@ console.log(input.attribute_value_id, "aaa")
                       name={"sub_category_id"}
                       value={input.sub_category_id}
                       onChange={handleInput}
-                      placeholder={"Select Product Sub Category"}
                       disabled={input.category_id == undefined}
                     >
                       <option>Select Sub Category</option>
@@ -202,33 +201,7 @@ console.log(input.attribute_value_id, "aaa")
                     </select>
                   </label>
                 </div>
-                <div className="col-md-2">
-                  <label className="w-100 mt-4 mt-md-0">
-                    <p>Select Attributes</p>
-                    <select
-                      className={"form-select mt-2"}
-                      name={"attribute_value_id"}
-                      value={input.attribute_value_id}
-                      onChange={(e) => {
-                        handleInput(e);
-                        const selectedAttr = attributes.find(attr => attr.attribute?.id === parseInt(e.target.value));
-                        setSelectedAttribute(input.attribute_value_id);
-                      }}
-                      placeholder={"Select attributes"}
-                    >
-                      <option>Select attributes</option>
-                      {attributes.map((attribute, index) => (
-                        <option value={attribute?.id} key={index}>
-                          {attribute.attributes.name} - {attribute.attribute_value.name}
-                        </option>
-                      ))}
-                    </select>
-
-
-                  </label>
-                </div>
-
-                <div className="col-md-3">
+                <div className="col-md-4">
                   <label className="w-100 mt-4 mt-md-0">
                     <p>Product Name</p>
                     <input
@@ -241,7 +214,7 @@ console.log(input.attribute_value_id, "aaa")
                     />
                   </label>
                 </div>
-                <div className="col-md-2 ">
+                <div className="col-md-2">
                   <div className="d-grid mt-4">
                     <button
                       onClick={handleProductSearch}
@@ -255,28 +228,48 @@ console.log(input.attribute_value_id, "aaa")
                   </div>
                 </div>
               </div>
-              <div className="row ">
-                <div className="col-md-3">
-                  <label className="w-100 mt-4 mt-md-0">
-                    <p>Column Count</p>
-                    <input
-                      className={"form-control mt-2"}
-                      type={"number"}
-                      name={"column_count"}
-                      value={columnCount}
-                      onChange={(e) => setColumnCount(parseInt(e.target.value))}
-                      placeholder={"Enter column count"}
-                    />
-                  </label>
-                </div>
-                <div className="col-md-3">
-                  <div className="d-grid mt-4">
-                    <button
-                      onClick={handlePrint}
-                      className={"btn btn-sm btn-success"}
-                    >
-                      Print This Out!
-                    </button>
+              <div className="container">
+                <div className="row my-2 d-flex justify-content-between align-items-center">
+                  <div className="col-md-4">
+                    <label className="w-100 mt-4 mt-md-0">
+                      <p>Column Count</p>
+                      <input
+                        className={"form-control mt-2"}
+                        type={"number"}
+                        name={"column_count"}
+                        value={columnCount}
+                        onChange={(e) => setColumnCount(parseInt(e.target.value))}
+                        placeholder={"Enter column count"}
+                      />
+                    </label>
+                  </div>
+                  <div className="col-md-4">
+                    <label className="w-100 mt-4 mt-md-0">
+                      <p>Select Attribute</p>
+                      <select
+                        className={"form-select mt-2"}
+                        name={"attribute_value_id"}
+                        value={input.attribute_value_id}
+                        onChange={handleInput}
+                      >
+                        <option>Select attributes</option>
+                        {attributes.map((attribute, index) => (
+                          <option value={attribute?.id} key={index}>
+                            {attribute.attributes.name} - {attribute.attribute_value.name}
+                          </option>
+                        ))}
+                      </select>
+                    </label>
+                  </div>
+                  <div className="col-md-4">
+                    <div className="d-grid">
+                      <button
+                        onClick={handlePrint}
+                        className={"btn btn-sm mt-3"}
+                      >
+                        <i class="fa-solid fa-print fa-beat-fade fa-2xl"></i>
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
@@ -287,11 +280,8 @@ console.log(input.attribute_value_id, "aaa")
                   printing={true}
                   rowCount={Math.ceil(products.length / columnCount)}
                   ref={componentRef}
-                  productSKU={productSKU}
-                  productName={productName}
-                  selectedAttribute={selectedAttributeObject}
+                  selectedAttribute={selectedAttribute}
                 />
-
               </div>
             </div>
           </div>
--- src/components/modules/bar_code/BarCodePage.js
+++ src/components/modules/bar_code/BarCodePage.js
@@ -1,99 +1,88 @@
-import React, { useEffect, useState } from 'react';
-import Barcode from 'react-barcode';
-import GlobalFunction from '../../../assets/GlobalFunction';
+import React from "react";
+import Barcode from "react-barcode";
+import GlobalFunction from "../../../assets/GlobalFunction";
 
 const TruncateText = (text, maxLength) => {
-    if (text.length > maxLength) {
-        return text.substring(0, maxLength) + '...';
-    }
-    return text;
+  if (text.length > maxLength) {
+    return text.substring(0, maxLength) + "...";
+  }
+  return text;
 };
 
+const BarCodePage = React.forwardRef((props, ref) => {
+  const { selectedAttribute, products, columnCount, printing } = props;
 
-// Inside BarCodePage component
-const BarCodePage = ({ products, columnCount, printing, rowCount, ref, productSKU, selectedAttribute }) => {
-    const [input, setInput] = useState({
-        name: productSKU || '', // Set the initial value to productSKU
-    });
+  const pages = [];
+  for (let i = 0; i < products.length; i += columnCount) {
+    const pageProducts = products.slice(i, i + columnCount);
+    pages.push(pageProducts);
+  }
 
-    const pages = [];
-    for (let i = 0; i < products.length; i += columnCount) {
-        const pageProducts = products.slice(i, i + columnCount);
-        pages.push(pageProducts);
-    }
-
-    useEffect(() => {
-    }, [selectedAttribute]);
-    // Define a function to get the selected attribute value from product_attributes
-    const getSelectedAttributeValue = (product, selectedAttr) => {
-        console.log("product:", product);
-        console.log("selectedAttr:", selectedAttr);
-      
-        if (!selectedAttr || !selectedAttr.attribute || !selectedAttr.attribute.attributes) {
-          console.log("Returning empty string");
-          return ''; // Return an empty string if selectedAttr or its properties are undefined
-        }
-      
-        const matchingAttribute = product.product_attributes.find(
-          attr => attr.attribute_id === selectedAttr.attribute.attributes.id
-        );
-      
-        if (matchingAttribute) {
-          return `${selectedAttr.attribute.attributes.name} - ${matchingAttribute.attribute_value.name}`;
-        }
-      
-        return '';
-      };
-      
-  
-
-    return (
-        <div className="print-page" style={{ width: '55mm', height: '25mm' }}>
-            {pages.map((page, pageIndex) => (
-                <div key={pageIndex} className="" ref={ref}>
-                    {page.map((product, index) => (
-                        <div
-                            key={index}
-                            className="bar-code-item"
-                            style={{
-                                flex: `1 0 ${100 / columnCount}%`,
-                                display: 'flex',
-                                flexDirection: 'column',
-                                alignItems: 'center',
-                                fontSize: '9px',
-                                letterSpacing: ".15em",
-                                margin: "0px"
-                            }}
-                        >
-                            <p>
-                                <small style={{ fontSize: "12px" }}><b>{product?.brand?.name}</b></small>
-                            </p>
-                            <div className="barcode" style={{ textAlign: 'center', format: "CODE128" }}>
-                                <Barcode value={product.sku} width={1} height={12} fontSize={8} margin={0} />
-                            </div>
-                            <p>
-                                <strong>{TruncateText(product?.name, 20)}</strong>
-                            </p>
-                            {getSelectedAttributeValue(product, selectedAttribute)}
-                            <p>
-                                <b>
-                                Price:
-                                {product?.sell_price?.discount !== 0
-                                    ? GlobalFunction.formatPrice(product?.sell_price?.price)
-                                    : ''}
-                                <span className={product?.sell_price?.discount !== 0 ? 'deleted ms-2' : ''}>
-                                    {product?.price} {" + VAT"}
-                                </span>
-                                </b>
-                            </p>
-                        </div>
-                    ))}
-                </div>
-            ))}
+  return (
+    <div className="print-page" style={{ width: "55mm", height: "25mm" }}>
+      {pages.map((page, pageIndex) => (
+        <div key={pageIndex} className="" ref={ref}>
+          {page.map((product, index) => (
+            <div
+              key={index}
+              className="bar-code-item"
+              style={{
+                flex: `1 0 ${100 / columnCount}%`,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                fontSize: "9px",
+                letterSpacing: ".15em",
+                margin: "0px",
+              }}
+            >
+              <p>
+                <small style={{ fontSize: "12px" }}>
+                  <b>{product?.brand?.name}</b>
+                </small>
+              </p>
+              <div
+                className="barcode"
+                style={{ textAlign: "center", format: "CODE128" }}
+              >
+                <Barcode
+                  value={product.sku}
+                  width={1}
+                  height={12}
+                  fontSize={8}
+                  margin={0}
+                />
+              </div>
+              <p>
+                <strong>{TruncateText(product?.name, 20)}</strong>
+              </p>
+                <p>
+                  <b>
+                    {selectedAttribute?.attributes?.name}:{" "}
+                    {selectedAttribute?.attribute_value?.name}
+                  </b>
+                </p>
+              <p>
+                <b>
+                  Price:
+                  {product?.sell_price?.discount !== 0
+                    ? GlobalFunction.formatPrice(product?.sell_price?.price)
+                    : ""}
+                  <span
+                    className={
+                      product?.sell_price?.discount !== 0 ? "deleted ms-2" : ""
+                    }
+                  >
+                    {product?.price} {" + VAT"}
+                  </span>
+                </b>
+              </p>
+            </div>
+          ))}
         </div>
-    );
-};
+      ))}
+    </div>
+  );
+});
 
 export default BarCodePage;
-
-
